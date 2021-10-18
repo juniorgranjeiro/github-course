f3655f62400cef4fe031da23f1cd514356570fb8
Junior Granjeiro <Junior Granjeiro>
2021-10-18 12:00:05 -0300

    ClientTableSeeder

--- projeto/database/seeds/DatabaseSeeder.php
+++ projeto/database/seeds/DatabaseSeeder.php
@@ -14,8 +14,11 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        
+
+        $this->call(ClientTableSeeder::class);
 
         Model::reguard();
     }
 }
+
